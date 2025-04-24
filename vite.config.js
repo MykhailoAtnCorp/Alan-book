f11849db639ef736d285e1c00694bb9a318cf0cd
Mykhailo Derevianko <mderevianko@atncorp.com>
2025-04-24 15:15:02 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,51 @@ import { resolve } from 'path'
 import FullReload from 'vite-plugin-full-reload'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import injectHTML from 'vite-plugin-html-inject'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
 	build: {
 		target: 'es2022',
 		outDir: 'dist',
-		assetsDir: 'assets'
+		assetsDir: 'assets',
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				'step-character': resolve(__dirname, 'step-character.html')
+			},
+			output: {
+				entryFileNames: (chunk) => {
+					const name = chunk.name.replace(/\.js$/, '')
+					return `assets/${name}.js`
+				},
+				chunkFileNames: 'assets/chunks/[name]-[hash].js',
+				assetFileNames: (assetInfo) => {
+					const info = assetInfo.name.split('.')
+					const extType = info[info.length - 1]
+
+				
+					if (extType === 'css') {
+				
+						let name = 'style' 
+
+				
+						if (assetInfo.name.includes('main')) {
+							name = 'main'
+						} else if (assetInfo.name.includes('step-character')) {
+							name = 'step-character'
+						}
+
+						return `assets/${name}.css`
+					}
+
+				
+					return 'assets/[name][extname]'
+				},
+				manualChunks: {}
+			}
+		},
+		cssCodeSplit: true
 	},
 	resolve: {
 		alias: {
@@ -24,7 +63,7 @@ export default defineConfig({
 		port: 3025,
 		host: '0.0.0.0',
 		hmr: true,
-		allowedHosts: ['16f1-188-130-177-205.ngrok-free.app']
+		allowedHosts: ['5594-188-130-177-205.ngrok-free.app']
 	},
 
 	plugins: [
@@ -44,6 +83,21 @@ export default defineConfig({
 				{
 					src: 'src/assets/*',
 					dest: 'assets'
+				}
+			]
+		}),
+		createHtmlPlugin({
+			minify: false,
+			pages: [
+				{
+					entry: 'src/js/main.js',
+					filename: 'index.html',
+					template: 'index.html'
+				},
+				{
+					entry: 'src/js/step-character.js',
+					filename: 'step-character.html',
+					template: 'step-character.html'
 				}
 			]
 		})
